e148f24599f9e06f6843d74354187e8b70d3c17e
ASurette <asurette@bu.edu>
2018-03-13 17:07:21 -0400

    Now creates the base array that will be our schedule

--- Scheduler.js
+++ Scheduler.js
@@ -1,11 +1,9 @@
 
+//Takes the time as a string and calculates where it should be in the array
 function array_position_calculation(hour)
 {
-   console.log(hour);
 
    hours_in_minutes = (Number(hour[0] * 10) + Number(hour[1])) * 60;
-
-   console.log('hours_in_minutes is', hours_in_minutes);
 
    minutes = (Number(hour[2])* 10) + Number(hour[3])
 
@@ -17,33 +15,32 @@ function array_position_calculation(hour)
 
 function array_creator(start_of_day, end_of_day){
 
-    var busy_array;
+    var busy_array = Array.apply(null, Array(96)).map(Number.prototype.valueOf,0);
 
-    for(i=0; i<96; i++){
-        busy_array[i] = 0;
+    array_placement_start = array_position_calculation(start_of_day);
+
+    array_placement_end = array_position_calculation(end_of_day);
+
+    for(i = 0; i < array_placement_start; i++)
+    {
+
+        busy_array[i] = 'B';//set all the values until they start their day as busy
+
     }
 
+    for(i = array_placement_end; i < 96; i++)
+    {
+
+        busy_array[i] = 'B'//the the values of when the user goes to sleep to busy
+
+    }
+
+    return busy_array;
 }
 
 function ScheduleAlgorithm(day_or_week, busy_array) {
         if(day_or_week == 0){//0 means the user asked for a day schedule
 
 
-
         }
 }
-
-console.log(array_position_calculation('0000'));
-
-console.log(array_position_calculation('0015'));
-
-console.log(array_position_calculation('0600'));
-
-console.log(array_position_calculation('1000'));
-
-console.log(array_position_calculation('0215'));
-
-console.log(array_position_calculation('1245'));
-
-console.log(array_position_calculation('2400'));
-
